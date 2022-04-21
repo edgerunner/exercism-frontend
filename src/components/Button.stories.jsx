f347774d6ef7be7b9e74f61a677f8956b9207209
Mert Torun <mert@merttorun.com>
2022-04-21 16:12:42 +0300

    Implement optional arrows for <Button>

--- src/components/Button.stories.jsx
+++ src/components/Button.stories.jsx
@@ -3,7 +3,10 @@ import Button from "./Button";
 export default {
   title: "Components/Button",
   component: Button,
-  argTypes: { onClick: { action: "clicked" } },
+  argTypes: {
+    onClick: { action: "clicked" },
+    arrow: { control: "inline-radio", options: [null, "left", "right"] },
+  },
 };
 
 const Template = (args) => <Button {...args} />;
@@ -17,4 +20,10 @@ export const Disabled = Template.bind({});
 Disabled.args = {
   children: "Disabled button",
   onClick: null,
+};
+
+export const Arrow = Template.bind({});
+Arrow.args = {
+  children: "Arrow",
+  arrow: "left",
 };
