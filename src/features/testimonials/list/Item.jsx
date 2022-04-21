8303e37c87d73bbc2cb3c60ea527454bb78b9b76
Mert Torun <mert@merttorun.com>
2022-04-21 19:10:54 +0300

    Use a passthrough <a> element for page navigation in <Testimonials.List.Item>

--- src/features/testimonials/list/Item.jsx
+++ src/features/testimonials/list/Item.jsx
@@ -10,28 +10,21 @@ export default function Item({
   created_at,
 }) {
   return (
-    <article
-      className="Testimonial"
-      id={`testimonial-${id}`}
-      onClick={navigateTo(id, mentor.handle)}
-    >
-      <TrackIcon src={track.icon_url} size="small" />
-      <Avatar src={mentor.avatar_url} handle={mentor.handle} />
-      <header>
-        <h5>{mentor.handle}</h5>
-        <h6>
-          on {exercise.title} in {track.title}
-        </h6>
-      </header>
-      <p>{content}</p>
-      <TimeAgo date={new Date(created_at)} />
+    <article className="Testimonial" id={`testimonial-${id}`}>
+      <a
+        href={`https://exercism.org/profiles/${mentor.handle}/testimonials?uuid=${id}`}
+      >
+        <TrackIcon src={track.icon_url} size="small" />
+        <Avatar src={mentor.avatar_url} handle={mentor.handle} />
+        <header>
+          <h5>{mentor.handle}</h5>
+          <h6>
+            on {exercise.title} in {track.title}
+          </h6>
+        </header>
+        <p>{content}</p>
+        <TimeAgo date={new Date(created_at)} />
+      </a>
     </article>
   );
-}
-
-function navigateTo(id, handle) {
-  return () =>
-    window.location.assign(
-      `https://exercism.org/profiles/${handle}/testimonials?uuid=${id}`
-    );
 }
