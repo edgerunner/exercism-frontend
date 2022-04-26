5b6f3289fc72e4893f972e1380a1d6eac33151c1
Mert Torun <mert@merttorun.com>
2022-04-26 16:53:30 +0300

    Export the <Input> component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
 export { default as Dropdown } from "./Dropdown";
+export { default as Input } from "./Input";
 export { default as PageNavigation } from "./PageNavigation";
 export { default as Pill } from "./Pill";
 export { default as TimeAgo } from "./TimeAgo";
