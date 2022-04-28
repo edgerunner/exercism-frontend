359b36ee734312c3f73aba7ff3c9ac9014c07d01
Mert Torun <mert@merttorun.com>
2022-04-28 14:14:09 +0300

    Build the <MyTestimonials> page component

---
+++ src/features/testimonials/MyTestimonials.jsx
@@ -0,0 +1,16 @@
+import List from "./List";
+import { Pill } from "/src/components";
+import "./MyTestimonials.css";
+
+export default function MyTestimonials(props) {
+  return (
+    <main className="MyTestimonials page">
+      <header>
+        <h1>Testimonials I've left</h1>
+        <Pill value={63} />
+        {/* dummy value to be replaced */}
+      </header>
+      <List {...props} />
+    </main>
+  );
+}
---
+++ src/features/testimonials/MyTestimonials.stories.jsx
@@ -0,0 +1,12 @@
+import MyTestimonials from "./MyTestimonials";
+import * as ListStories from "./List/index.stories";
+
+export default {
+  title: "Features/Testimonials/MyTestimonials",
+  component: MyTestimonials,
+};
+
+const Template = (args) => <MyTestimonials {...args} />;
+
+export const Full = Template.bind({});
+Full.args = ListStories.Full.args;
