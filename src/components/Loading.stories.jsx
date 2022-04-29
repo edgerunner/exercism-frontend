15a8c063836962c75b8b60539a06be61f33e3a60
Mert Torun <mert@merttorun.com>
2022-04-29 15:55:17 +0300

    Differentiate between scoped and unscoped <Loading> spinners

--- src/components/Loading.stories.jsx
+++ src/components/Loading.stories.jsx
@@ -3,18 +3,33 @@ import Loading from "./Loading";
 export default {
   title: "Components/Loading",
   component: Loading,
+  argTypes: {
+    scoped: {
+      description:
+        "This is not a real prop. If a parent element has `Loading-scope` class, then the component will be scoped.",
+      control: {
+        type: "boolean",
+      },
+      defaultValue: false,
+    },
+  },
 };
 
-export function Overlay() {
+export function Overlay({ scoped }) {
   return (
-    <div className="example">
+    <div className={scoped ? "Loading-scope" : null}>
       <h1>Loading</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris euismod,
-        nunc euismod euismod consectetur, nisl nisl euismod nisl, euismod
-        euismod nisl nisl euismod.
-      </p>
+      {[1, 2, 3, 4].map((i) => (
+        <p key={i}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          euismod, nunc euismod euismod consectetur, nisl nisl euismod nisl,
+          euismod euismod nisl nisl euismod.
+        </p>
+      ))}
       <Loading />
     </div>
   );
 }
+Overlay.args = {
+  scoped: false,
+};
