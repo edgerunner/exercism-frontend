2b8fa7e7b8a40adb9a5768d74c08a2c284221817
Mert Torun <mert@merttorun.com>
2022-05-06 22:55:46 +0300

    Populate the pill in the heading with sum from the API

--- src/features/testimonials/MyTestimonials.jsx
+++ src/features/testimonials/MyTestimonials.jsx
@@ -1,5 +1,6 @@
 import List from "./list";
 import { Pill } from "/src/components";
+import { useMemo } from "react";
 import { useMachine } from "@xstate/react";
 import machine from "./MyTestimonials.machine.js";
 import "./MyTestimonials.css";
@@ -8,12 +9,20 @@ export default function MyTestimonials(props) {
   const [state] = useMachine(machine);
   console.log(state);
 
+  const totalCount = useMemo(
+    () =>
+      state.context.testimonials?.tracks.reduce(
+        (sum, track) => sum + track.count,
+        0
+      ),
+    [state]
+  );
+
   return (
     <main className="MyTestimonials page">
       <header>
         <h1>Testimonials I&apos;ve left</h1>
-        <Pill value={63} />
-        {/* dummy value to be replaced */}
+        <Pill value={totalCount} />
       </header>
       <List {...props} />
     </main>
