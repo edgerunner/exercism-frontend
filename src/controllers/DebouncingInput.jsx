b696c8cd0b2575063e8092e7715db0946a29f2ad
Mert Torun <mert@merttorun.com>
2022-05-08 11:55:14 +0300

    Implement a <DebouncingInput> controller

---
+++ src/controllers/DebouncingInput.jsx
@@ -0,0 +1,71 @@
+import { createMachine, assign } from "xstate";
+import { useMachine } from "@xstate/react";
+import { useMemo, useCallback } from "react";
+
+/** @xstate-layout N4IgpgJg5mDOIC5QBEwCMD2BXAdgYwEscoBJHABywBcA6VTXQ4gYgA9YqBDKsGzgMx4AnABQQwAG04BPAJTN62fEVIVqddEqZREochlgEqBDDl0hWiAKwAGGgHYAnAA57Nx04DMANgCMjgBZnABoQaUQAWgAmTxoA72dfH3tfKICo7zdvAF9s0MVGFTJKWmLqZnN9Q2NTc0sEKKj7OO8bK1arFxsbewCrUPCEJLsg+ytEm083Tytc-M1C4jKqZlNlyoMjEzMkC0RPTzsrXxtfANOAs89HHwHImJbEgL7z1pnPKNy8kBwMcXhdgVlEs1KUIBIwBtqts6ohejRnG1XDYMod0t47ggIr4zjQ-IdfFYPvFnO1PHMQEDtMsNAxgTpdlUtrVdvV4giojZnE1uYTTqdMalYi4fN4+p4+glSRSqUVQTR1ozNjUdqB6gdnHEkvZvMd-AEdb5BQ8RZl4mlvGKkjKFvTFXplTDWYhGhyuTyonyTklMdjHI48ZNHO4Uf4mjkvkA */
+const machine = createMachine(
+  {
+    context: { value: "" },
+    id: "DebouncingInput",
+    on: {
+      onInput: {
+        actions: "updateValue",
+        target: ".Input",
+      },
+    },
+    initial: "Idle",
+    states: {
+      Idle: {},
+      Debouncing: {
+        after: {
+          delay: {
+            actions: "fireChangeEvent",
+            target: "Idle",
+          },
+        },
+      },
+      Input: {
+        always: {
+          target: "Debouncing",
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      updateValue: assign({ value: (_, event) => event.value }),
+    },
+  }
+);
+
+export default function DebouncingInput({
+  delay = 500,
+  defaultValue = "",
+  onChange,
+  children,
+}) {
+  const options = useMemo(
+    () => ({
+      delays: { delay },
+      actions: {
+        fireChangeEvent: (context) => onChange?.(context.value),
+      },
+    }),
+    [delay, onChange]
+  );
+  const [state, send] = useMachine(
+    machine.withContext({ value: defaultValue }),
+    options
+  );
+
+  const handleInput = useCallback(
+    (value) => send("onInput", { value }),
+    [send]
+  );
+
+  return children(
+    state.context.value,
+    handleInput,
+    state.matches("Debouncing")
+  );
+}
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,2 @@
+export { default as DebouncingInput } from "./DebouncingInput";
 export { default as Pagination } from "./Pagination";
