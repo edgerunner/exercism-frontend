c5123662d1c5f150432fe61efc40924519e8b99e
Mert Torun <mert@merttorun.com>
2022-05-09 13:25:24 +0300

    Memoize machine send actions in <MyTestimonials>

--- src/features/testimonials/MyTestimonials.jsx
+++ src/features/testimonials/MyTestimonials.jsx
@@ -1,11 +1,23 @@
 import List from "./list";
 import { Pill, Loading } from "/src/components";
+import { useMemo } from "react";
 import { useMachine } from "@xstate/react";
 import machine from "./MyTestimonials.machine.js";
 import "./MyTestimonials.css";
 
 export default function MyTestimonials() {
   const [state, send] = useMachine(machine);
+
+  const events = useMemo(
+    () => ({
+      onPageChange: (page) => send("Change page", { page }),
+      onTrackChange: (track) => send("Change track selection", { track }),
+      onExerciseChange: (exercise) =>
+        send("Change exercise search", { exercise }),
+      onOrderChange: (order) => send("Change ordering", { order }),
+    }),
+    [send]
+  );
 
   if (state.matches("Loading"))
     return (
@@ -24,16 +36,14 @@ export default function MyTestimonials() {
         testimonials={state.context.testimonials.results}
         currentPage={state.context.testimonials.pagination.current_page}
         pageCount={state.context.testimonials.pagination.total_pages}
-        onPageChange={(page) => send("Change page", { page })}
+        onPageChange={events.onPageChange}
         tracks={state.context.testimonials.tracks}
-        onTrackChange={(track) => send("Change track selection", { track })}
+        onTrackChange={events.onTrackChange}
         selectedTrack={state.context.parameters.track}
         exercise={state.context.parameters.exercise}
-        onExerciseChange={(exercise) =>
-          send("Change exercise search", { exercise })
-        }
+        onExerciseChange={events.onExerciseChange}
         order={state.context.parameters.order}
-        onOrderChange={(order) => send("Change ordering", { order })}
+        onOrderChange={events.onOrderChange}
         loading={state.matches("Ready.Loading")}
       />
     </main>
