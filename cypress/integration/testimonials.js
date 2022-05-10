424c65ec9e42a0cc0f7bea77a6c3e396d4897f43
Mert Torun <mert@merttorun.com>
2022-05-10 19:15:51 +0300

    Test: My testimonials should display only testimonials of a selected track

--- cypress/integration/testimonials.js
+++ cypress/integration/testimonials.js
@@ -8,7 +8,23 @@ describe("My Testimonals", () => {
     cy.get("@testimonials").should("have.length", 20);
   });
 
-  it("should display only testimonials of a selected track");
+  it("should display only testimonials of a selected track", () => {
+    cy.intercept({
+      method: "GET",
+      pathname: "/api/v2/hiring/testimonials",
+      query: { track: "javascript" },
+    }).as("api-call");
+    cy.get(".TrackSelector")
+      .click()
+      .within(() => {
+        cy.contains("JavaScript").click();
+      });
+    cy.wait("@api-call");
+    cy.get("@testimonials").each(($testimonial) => {
+      cy.wrap($testimonial).get("h6").contains("JavaScript");
+    });
+  });
+
   it("should display ordered results");
   it("should filter by exercise");
 
